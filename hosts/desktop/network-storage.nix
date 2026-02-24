a35e8de466396b5c63ee9f724b7bca97a3e597ab
Michael McMillan <mcmillanbmac@gmail.com>
2026-02-24 17:11:55 +1100

    nixos-desktop: Update Gen 10 | Modified: hosts/desktop/network-storage.nix

--- hosts/desktop/network-storage.nix
+++ hosts/desktop/network-storage.nix
@@ -7,13 +7,13 @@
   fileSystems."/mnt/nas" = {
     device = "192.168.8.3:/volume1/University"; # Verify your actual shared folder path
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" ];
+    options = [ "rw" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" ];
   };
 
   # Mini PC (Proxmox) Mount
   fileSystems."/mnt/proxmox" = {
     device = "192.168.8.2:/home/michael/University/USQ"; # Only if you have NFS exports set up on PVE
     fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" ];
+    options = [ "rw" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" "soft" ];
   };
 }
