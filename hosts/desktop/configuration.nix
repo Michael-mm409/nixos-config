cc5358abc68fcf432b9cbb97cfaa16fa8dac7a84
Michael McMillan <mcmillanbmac@gmail.com>
2026-03-01 11:53:10 +1100

    nixos-desktop: Update Gen 31 | Modified: hosts/desktop/configuration.nix

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -14,7 +14,6 @@
 
   boot.loader.systemd-boot = {
   	enable = true;
-        configurationLimit = 5; # Keeps only the 5 most recent NixOS generations
   	# This stops systemd-boot from creating its own 'auto-windows' entry
   	# so only your '00-windows' manual entry shows up.
   	extraEntries = {
